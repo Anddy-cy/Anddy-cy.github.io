54814417a0389844b0004f7fea59a20734c4d2a3
andlyf <60910372+Anddy-cy@users.noreply.github.com>
2022-03-09 18:28:26 +0800

    Update url.js

--- js/url.js
+++ js/url.js
@@ -1,9 +1,10 @@
 function ceshi(){
 	alert("这是一个测试");
 }
-var aseKey = "12345678"     //秘钥必须为：8/16/32位
+
 
 function jm1(){	
+	var aseKey = "12345678"     //秘钥必须为：8/16/32位
         var message = "http://[2409:8a48:2442:6fe1:9cc5:ba1a:9dfa:da00]/";
         //加密
         encrypt = CryptoJS.AES.encrypt(message, CryptoJS.enc.Utf8.parse(aseKey), {
@@ -14,7 +15,7 @@ function jm1(){
 	alert(encrypt);
 }
 
-function jm2(){
+function jm2(aseKey){
 	//解密
         var decrypt = CryptoJS.AES.decrypt(encrypt, CryptoJS.enc.Utf8.parse(aseKey), {
                 mode: CryptoJS.mode.ECB,
@@ -22,6 +23,12 @@ function jm2(){
         }).toString(CryptoJS.enc.Utf8);
         //console.log(decrypt);    //80018000142
 	alert(decrypt);
+}
+
+function shuru(){
+	var aseKey=prompt("暗号:","");
+	jm2(aseKey);
+
 }
 	
 	
