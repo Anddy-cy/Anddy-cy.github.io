569abdbbae38ff67bc2e5c65a1b5748386532a8b
andlyf <60910372+Anddy-cy@users.noreply.github.com>
2022-03-09 18:04:46 +0800

    Update url.js

--- js/url.js
+++ js/url.js
@@ -1,3 +1,20 @@
 function ceshi(){
 	alert("这是一个测试");
 }
+
+
+var aseKey = "12345678"     //秘钥必须为：8/16/32位
+var message = "80018000142";
+//加密
+var encrypt = CryptoJS.AES.encrypt(message, CryptoJS.enc.Utf8.parse(aseKey), {
+  mode: CryptoJS.mode.ECB,
+  padding: CryptoJS.pad.Pkcs7
+}).toString();
+console.log(encrypt);    //VKrZlqykem73x8/T2oCfCQ==
+
+//解密
+var decrypt = CryptoJS.AES.decrypt(encrypt, CryptoJS.enc.Utf8.parse(aseKey), {
+  mode: CryptoJS.mode.ECB,
+  padding: CryptoJS.pad.Pkcs7
+}).toString(CryptoJS.enc.Utf8);
+console.log(decrypt);    //80018000142
