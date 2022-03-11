315d0b624ecd892675bffc0e6b84d1ab734fc382
andlyf8 <lyfzwcyy@foxmail.com>
2022-03-11 14:52:19 +0800

    “提交信息”

--- js/url.js
+++ js/url.js
@@ -2,7 +2,6 @@ function ceshi(){
 	alert("这是一个测试");
 }
 
-
 // function jm1(){	
 // 	var aseKey = "12345678"     //秘钥必须为：8/16/32位
 //         var message = "http://[2409:8a48:2442:6fe1:9cc5:ba1a:9dfa:da00]/";
@@ -15,23 +14,18 @@ function ceshi(){
 // 	alert(encrypt);
 // }
 
-function jm2(aseKey){
-	//解密
-        var decrypt = CryptoJS.AES.decrypt(encrypt, CryptoJS.enc.Utf8.parse(aseKey), {
-                mode: CryptoJS.mode.ECB,
-                padding: CryptoJS.pad.Pkcs7
-        }).toString(CryptoJS.enc.Utf8);
-        //console.log(decrypt);    //80018000142
-	alert(decrypt);
-}
+// function jm2(aseKey){
+// 	//解密
+//         var decrypt = CryptoJS.AES.decrypt(encrypt, CryptoJS.enc.Utf8.parse(aseKey), {
+//                 mode: CryptoJS.mode.ECB,
+//                 padding: CryptoJS.pad.Pkcs7
+//         }).toString(CryptoJS.enc.Utf8);
+//         //console.log(decrypt);    //80018000142
+// 	alert(decrypt);
+// }
 
-function shuru(){
-	var aseKey=prompt("暗号:","");
-	jm2(aseKey);
+// function shuru(){
+// 	var aseKey=prompt("暗号:","");
+// 	jm2(aseKey);
 
-}
-	
-	
-	
-	
-
+// }
