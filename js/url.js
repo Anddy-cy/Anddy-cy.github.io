ea0f7b9a81ebaf563d820cfaf093fb97b959430c
andlyf8 <lyfzwcyy@foxmail.com>
2022-03-11 23:22:00 +0800

    周五 2022/03/11 23:22:00.24

--- js/url.js
+++ js/url.js
@@ -3,14 +3,14 @@ function ceshi(){
 }
 
 // function jm1(){	
-// 	var aseKey = "12345678"     //秘钥必须为：8/16/32位
-//         var message = "http://[2409:8a48:2442:6fe1:9cc5:ba1a:9dfa:da00]/";
+// 	var aseKey = ""     //秘钥必须为：8/16/32位
+//         var message = ""; //待加密的内容
 //         //加密
 //         encrypt = CryptoJS.AES.encrypt(message, CryptoJS.enc.Utf8.parse(aseKey), {
 //                 mode: CryptoJS.mode.ECB,
 //                 padding: CryptoJS.pad.Pkcs7
 //         }).toString();
-//         //console.log(encrypt);    //VKrZlqykem73x8/T2oCfCQ==
+//         //console.log(encrypt);    
 // 	alert(encrypt);
 // }
 
@@ -20,7 +20,7 @@ function ceshi(){
 //                 mode: CryptoJS.mode.ECB,
 //                 padding: CryptoJS.pad.Pkcs7
 //         }).toString(CryptoJS.enc.Utf8);
-//         //console.log(decrypt);    //80018000142
+//         //console.log(decrypt);  
 // 	alert(decrypt);
 // }
 
